599c40d43547c24c371bd8373020b373c42e9569
Chelsea Valentine <cv851@nyu.edu>
2016-11-26 05:10:12 -0500

    Display search results.

--- src/components/MusicSearch/MusicSearch.jsx
+++ src/components/MusicSearch/MusicSearch.jsx
@@ -1,8 +1,14 @@
+import $ from 'jquery';
 import React from 'react';
 
+import { SPOTIFY_API } from '../../js/constants';
 import './style.scss';
 
 const MusicSearch = React.createClass({
+  getInitialState() {
+    return { results: [] };
+  },
+
   componentDidMount() {
     this.input.addEventListener('keyup', (e) => {
       if (e.keyCode === 13) {
@@ -20,6 +26,42 @@ const MusicSearch = React.createClass({
       this.displayError('Please specify a song to search for.');
       return;
     }
+
+    const SEARCH_LIMIT = 3;
+    const SEARCH_TYPE = 'track';
+
+    // TODO: Add loading/searching indicator
+
+    $.ajax({
+      type: 'GET',
+      datatype: 'json',
+      url: SPOTIFY_API.SEARCH + '?q=' + query + '&limit=' + SEARCH_LIMIT + '&type=' + SEARCH_TYPE,
+      // TODO: Get an API key and use it here
+    })
+    .done((response, status, jqXHR) => {
+      const results = response.tracks.items;
+
+      if (results.length === 0) {
+        this.displayError('Spotify couldn\'t find any matches for the query, \'' + query + '\'');
+        return;
+      }
+
+      this.displayResults(results);
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+      this.displayError('The Spotify API seems to be done! :(');
+    });
+  },
+
+  displayResults(results) {
+    // TODO: remove loading/searching indicator
+    this.input.classList.add('musicSearch-loaded');
+    this.setState({ results: results });
+  },
+
+  removeResults() {
+    this.input.classList.remove('musicSearch-loaded');
+    this.setState({ results: [] });
   },
 
   displayError(message) {
@@ -35,7 +77,9 @@ const MusicSearch = React.createClass({
 
   render() {
     return (
-      <div className="musicSearch">
+      <div
+        ref={ref => this.search = ref}
+        className="musicSearch">
         <input
           ref={ref => this.input = ref}
           type="text"
@@ -44,7 +88,35 @@ const MusicSearch = React.createClass({
         <p
           ref={ref => this.error = ref}
           className="musicSearch__error"></p>
-        <div className="musicSearch__results">
+
+        <div
+          ref={ref => this.results = ref }
+          className="musicSearch__results">
+          {this.state.results.map((result) => {
+            const albumCover = result.album.images[2] || {};
+            const songTitle = result.name;
+
+            // TODO: Join all artists together instead of using first
+            const artist = result.artists[0].name || '';
+
+            return (
+              <div className="musicSearch__result">
+                <img
+                  className="musicSearch__album"
+                  src={albumCover.url || ''}
+                  height={albumCover.height}
+                  width={albumCover.width} />
+
+                <p className="musicSearch__song">
+                  {songTitle}
+                </p>
+
+                <p className="musicSearch__artist">
+                  {artist}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
--- src/js/constants.jsx
+++ src/js/constants.jsx
@@ -1,1 +1,5 @@
+export const SPOTIFY_API = {
+  SEARCH: 'https://api.spotify.com/v1/search',
+};
+
 export const MAX_POST_LEN = 140;
