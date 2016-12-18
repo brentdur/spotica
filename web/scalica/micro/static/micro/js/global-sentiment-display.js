9efc65e05f1ed7a1d89d41047123e7d3c91ebcc7
Savannah <savslim@gmail.com>
2016-12-17 21:39:50 -0500

    get random JSON file to work with graph

--- web/scalica/micro/static/micro/js/global-sentiment-display.js
+++ web/scalica/micro/static/micro/js/global-sentiment-display.js
@@ -1,13 +1,19 @@
-d3.json('ufo-sightings.json', function(data) {
-    MG.data_graphic({
-        title: "UFO Sightings",
-        description: "Yearly UFO sightings from the year 1945 to 2010.",
-        data: data,
-        width: 650,
-        height: 150,
-        target: '#ufo-sightings',
-        x_accessor: 'year',
-        y_accessor: 'sightings',
-        markers: [{'year': 1964, 'label': '"The Creeping Terror" released'}]
+$(document).ready(function() {
+    d3.json('/static/micro/js/ufo-sightings.json', function(data) {
+        MG.data_graphic({
+            title: "UFO Sightings",
+            description: "Yearly UFO sightings from the year 1945 to 2010.",
+            // data: data,
+            data: data,
+            width: 650,
+            height: 150,
+            target: '#ufo-sightings',
+            x_accessor: 'year',
+            y_accessor: 'sightings',
+            markers: [{
+                'year': 1964,
+                'label': '"The Creeping Terror" released'
+            }]
+        })
     })
 })
