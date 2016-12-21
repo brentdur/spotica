19a0163aadec97372b676dbc264e56b33efa13b7
freialobo <freialisa@gmail.com>
2016-12-21 15:45:06 -0500

    SO MUCH MUSIC

--- web/scalica/utils/seed.py
+++ web/scalica/utils/seed.py
@@ -21,7 +21,8 @@ for name in names:
 
 sp = spotipy.Spotify()
 
-albums = ["spotify:album:4pC2URLdvle8V6Um4qxh46", "spotify:album:0cbpcdI4UySacPh5RCpDfo", "spotify:album:4nKfZbCALT9H9LfedtDwnZ", "spotify:album:5tbykpeXPR1kEeveI0xOzK", "spotify:album:5NOhXlXwlcGhRbfDWQRij0", "spotify:album:3539EbNgIdEDGBKkUf4wno", "spotify:album:1Od4S26RB37khls09dsUxL"]
+albums = ["spotify:album:4pC2URLdvle8V6Um4qxh46", "spotify:album:0cbpcdI4UySacPh5RCpDfo", "spotify:album:4nKfZbCALT9H9LfedtDwnZ", "spotify:album:5tbykpeXPR1kEeveI0xOzK", "spotify:album:5NOhXlXwlcGhRbfDWQRij0", "spotify:album:3539EbNgIdEDGBKkUf4wno", "spotify:album:1Od4S26RB37khls09dsUxL", "spotify:album:0K4pIOOsfJ9lK8OjrZfXzd", "spotify:album:1azUkThwd2HfUDdeNeT147", "spotify:album:6Pb3K1oPXdhsqFXtzKe3Z1", "spotify:album:3D4FYzqPi2LqJWyfsY1ZdI", "spotify:album:2B2xiybenVwWzkGclQQkzi","spotify:album:7gmak9ZGm10y4PtZa9SBQn","spotify:album:5JXp0gHTV2oA4DsLSixK1y","spotify:album:2ODvWsOgouMbaA5xf0RkJe","spotify:album:0P3oVJBFOv3TDXlYRhGL7s","spotify:album:3cfAM8b8KqJRoIzt3zLKqw","spotify:album:2G4AUqfwxcV1UdQjm2ouYr","spotify:album:2R7iJz5uaHjLEVnMkloO18","spotify:album:4XTT0NcNHyvl6h9JX2AfEi","spotify:album:4E7bV0pzG0LciBSWTszra6",
+"spotify:album:78bpIziExqiI9qztvNFlQu","spotify:album:6fC6BRXbuHSVobwWcwe6M7","spotify:album:2whKuzNvFl6OnkiOvN1vA0","spotify:album:0pvhletDH7CphbKErUtPCF","spotify:album:6odcotWv2xd7NP7RrGBS5b","spotify:album:6oX0gpxTxSrCZ1QJikDhMN","spotify:album:0vGxxgC1zHtpE9YXJ2p3aj","spotify:album:6tJBLSMVDUizDX86QDiYzg","spotify:album:6Fr2rQkZ383FcMqFyT7yPr","spotify:album:3OZgEywV4krCZ814pTJWr7","spotify:album:2UJwKSBUz6rtW4QLK74kQu","spotify:album:6RS1XvMUnj5HfHunnNzSSW","spotify:album:7pomP86PUhoJpY3fsC0WDQ"]
 
 for album in sp.artist_albums('spotify:artist:0du5cEVh5yTK9QJze8zA0C')['items']:
 	albums.append(album['uri'])
